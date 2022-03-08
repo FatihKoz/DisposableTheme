8a0fcf0979ae034aeb6876840c26d036304561d0
B.Fatih KOZ <fatih.koz@gmail.com>
2022-03-08 22:11:29 +0300

    Update create.blade.php
    Check theme settings and disable the form too

--- resources/views/layouts/Disposable_v3/pireps/create.blade.php
+++ resources/views/layouts/Disposable_v3/pireps/create.blade.php
@@ -7,25 +7,29 @@
 @endphp
 
 @section('content')
-  <div class="row">
-    <div class="col">
-      <div class="card mb-2">
-        <div class="card-header p-1">
-          <h5 class="m-1">
-            @lang('pireps.newflightreport')
-            <i class="fas fa-file-upload float-end"></i>
-          </h5>
+  @if(Theme::getSetting('pireps_manual'))
+    <div class="row">
+      <div class="col">
+        <div class="card mb-2">
+          <div class="card-header p-1">
+            <h5 class="m-1">
+              @lang('pireps.newflightreport')
+              <i class="fas fa-file-upload float-end"></i>
+            </h5>
+          </div>
+          @if(!empty($pirep))
+            {{ Form::model($pirep, ['route' => 'frontend.pireps.store']) }}
+          @else
+            {{ Form::open(['route' => 'frontend.pireps.store']) }}
+          @endif
+          @include('pireps.fields')
+          {{ Form::close() }}
         </div>
-        @if(!empty($pirep))
-          {{ Form::model($pirep, ['route' => 'frontend.pireps.store']) }}
-        @else
-          {{ Form::open(['route' => 'frontend.pireps.store']) }}
-        @endif
-        @include('pireps.fields')
-        {{ Form::close() }}
       </div>
     </div>
-  </div>
+  @else 
+    <div class="alert alert-danger mb-1 p-1 px-2 fw-bold">Manual Pilot/Flight Reports are disabled</div>
+  @endif
 @endsection
 
 @include('pireps.scripts')
