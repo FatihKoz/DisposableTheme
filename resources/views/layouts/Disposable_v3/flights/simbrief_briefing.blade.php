7e97c565b3d1bdb6a54f0b9f6a3e1dfeec3c5fcc
B.Fatih KOZ <fatih.koz@gmail.com>
2022-03-10 13:10:16 +0300

    Update simbrief_briefing.blade.php

--- resources/views/layouts/Disposable_v3/flights/simbrief_briefing.blade.php
+++ resources/views/layouts/Disposable_v3/flights/simbrief_briefing.blade.php
@@ -54,14 +54,14 @@
       <div class="d-flex align-items-start">
         <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
           @if($simbrief->flight_id && $user->id == $simbrief->user_id && $acars_plugin)
-            <a class="btn btn-sm btn-success my-1" href="@if(isset($bid)) {{'vmsacars:bid/'.$bid->id}} @else {{'vmsacars:flight/'.$simbrief->flight_id}} @endif">vmsAcars</a>
+            <a class="btn btn-sm btn-success my-1" href="@if(isset($bid)) {{'vmsacars:bid/'.$bid->id}} @else {{'vmsacars:flight/'.$simbrief->flight_id}} @endif">Acars</a>
           @endif
           <a class="btn btn-sm btn-primary my-1" data-bs-toggle="modal" data-bs-target="#ofp-view" href="#">View</a>
           @if(filled($simbrief->xml->params->static_id) && $user->id == $simbrief->user_id && $simbrief->flight_id)
             <a class="btn btn-sm btn-warning my-1" data-bs-toggle="modal" data-bs-target="#ofp-edit" href="#" onclick="OpenEditPage()">Edit</a>
           @endif
           @if($user->id == $simbrief->user_id && $simbrief->flight_id)
-            <a class="btn btn-sm btn-danger my-1" href="{{ url(route('frontend.simbrief.generate_new', [$simbrief->id])) }}">Generate New</a>
+            <a class="btn btn-sm btn-danger my-1" href="{{ url(route('frontend.simbrief.generate_new', [$simbrief->id])) }}">Delete<br>Generate New</a>
           @endif
           @if(!$simbrief->pirep_id && $user->id == $simbrief->user_id && Theme::getSetting('pireps_manual'))
             <a class="btn btn-sm btn-info my-1" href="{{ url(route('frontend.simbrief.prefile', [$simbrief->id])) }}">Manual PIREP</a>
