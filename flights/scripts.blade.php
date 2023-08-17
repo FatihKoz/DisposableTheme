6d2c10a66def3f6b17f3834ab1da1d69d48cd982
Doug <75090523+dougjuk@users.noreply.github.com>
2023-08-17 12:29:27 +0100

    Add files via upload
    Just a few small changes to get the airport search running in the Dispo theme.  Scripts taken from default theme.  Made two changes to search blade.

---
+++ flights/scripts.blade.php
@@ -0,0 +1,27 @@
+@section('scripts')
+<script>
+  $(document).ready(function () {
+    $("button.save_flight").click(async function (e) {
+      e.preventDefault();
+
+      const btn = $(this);
+      const class_name = btn.attr('x-saved-class'); // classname to use is set on the element
+      const flight_id = btn.attr('x-id');
+
+      if (!btn.hasClass(class_name)) {
+        await phpvms.bids.addBid(flight_id);
+        btn.addClass(class_name)
+        alert('@lang("flights.bidadded")');
+      } else {
+        await phpvms.bids.removeBid(flight_id);
+        btn.removeClass(class_name);
+        alert('@lang("flights.bidremoved")');
+      }
+    });
+  });
+
+</script>
+
+@include('scripts.airport_search')
+
+@endsection
