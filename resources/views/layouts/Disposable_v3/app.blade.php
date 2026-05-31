094c887d3c599cde957cced35f923cc1b04ae829
B.Fatih KOZ <fatih.koz@gmail.com>
2026-05-31 17:43:01 +0300

    Update app.blade.php

--- resources/views/layouts/Disposable_v3/app.blade.php
+++ resources/views/layouts/Disposable_v3/app.blade.php
@@ -10,7 +10,8 @@
     <meta name="api-key" content="{!! Auth::check() ? Auth::user()->api_key: '' !!}">
     <meta name="csrf-token" content="{!! csrf_token() !!}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@400;800">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5UEgmacK8SfF1V5VnC5pG1k6z4hNk0aQqY8x0k9x0k9x0" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/1310cf8385.js" crossorigin="anonymous"></script>
     <link href="{{ public_asset('/assets/global/css/vendor.css') }}" rel="stylesheet"/>
     <link href="{{ public_asset('/disposable/stylesheet/theme_v3.css?ver=1') }}" rel="stylesheet"/>
@@ -145,7 +146,8 @@
           }
         </script>
       @endif
-    {{-- Bootstrap Scripts v5.1.3 CDN --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    {{-- End of the UTC Clock code --}}
+    {{-- Bootstrap Scripts v5.3.3 CDN --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tYfcbuBHvYlZYYt+zBC农btMCYZ3ONnBR47/Os+XE120OVV6hO1VPknHm" crossorigin="anonymous"></script>
   </body>
 </html>
